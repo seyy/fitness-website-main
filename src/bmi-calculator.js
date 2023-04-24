5d8dece0073d40ce9eeba9cb8b30a93e887dd3c9
kacper.pieron <kpieron@poczta.fm>
2023-04-24 17:03:57 +0200

    Update bmi-calculator.js

--- src/bmi-calculator.js
+++ src/bmi-calculator.js
@@ -2,17 +2,22 @@ const weightInput = document.getElementById("weight");
 const heightInput = document.getElementById("height");
 const ageInput = document.getElementById("age");
 const calculateBtn = document.getElementById("calculateButton");
+const calculatorDiv = document.getElementsByClassName("bmiCalcBox");
+
+let bmiFirstPart;
+let finalBmi;
+
 
 ageInput.addEventListener("input", function () {
   console.log(age.value)
 })
 
 weightInput.addEventListener("input", function () {
-  console.log(weightInput)
+  console.log(weightInput.value)
 })
 
 heightInput.addEventListener("input", function () {
-  console.log(heightInput)
+  console.log(heightInput.value)
 })
 
 // HAMBURGER -------------------------------------------------------------
@@ -32,6 +37,19 @@ closeToggle.addEventListener('click', () => {
 
 // -------------------------------------------------------------------------
 
+// BMI FORMULA - BMI CALCULATING
+
+const bmiCalculation = () => {
+  bmiFirstPart = heightInput.value * heightInput.value / 100;
+  console.log(bmiFirstPart)
+  finalBmi = weightInput.value / bmiFirstPart * 100;
+  finalBmi = Math.floor(finalBmi);
+  console.log(finalBmi)
+  
+}
+
+//
+calculateBtn.addEventListener("click", bmiCalculation)
 
 
 
