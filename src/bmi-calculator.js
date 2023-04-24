fba87706d921660e6fbd51f1355fe7d5e78a2fc3
kacper.pieron <kpieron@poczta.fm>
2023-04-24 21:54:08 +0200

    Update bmi-calculator.js

--- src/bmi-calculator.js
+++ src/bmi-calculator.js
@@ -1,6 +1,5 @@
 const weightInput = document.getElementById("weight");
 const heightInput = document.getElementById("height");
-const ageInput = document.getElementById("age");
 const calculateBtn = document.getElementById("calculateButton");
 const calculatorDiv = document.getElementById("bmiCalcBox");
 const genderChoice = document.querySelectorAll('input[name="gender"]');
@@ -25,7 +24,35 @@ closeToggle.addEventListener('click', () => {
 
 // -------------------------------------------------------------------------
 
+weightInput.addEventListener("input", function() {
+  if(weightInput.value && !heightInput.value) {
+    calculateBtn.disabled = true;
+  } else {
+    calculateBtn.disabled = false;
+  }
+});
 
+heightInput.addEventListener("input", function() {
+  if(heightInput.value && !weightInput.value) {
+    calculateBtn.disabled = true;
+  } else {
+    calculateBtn.disabled = false;
+  }
+});
+
+// ADDS A BUTTON THAT ON CLICK YOU COME BACK TO THE FORM OF BMI CALCULATOR
+
+  const calculateAgainBtn = document.createElement("button");
+  calculateAgainBtn.innerText = "Calculate Again";
+  calculateAgainBtn.classList.add("calcAgainBtn");
+
+  calculateAgainBtn.addEventListener("click", function() {
+    calculateAgainBtn.disabled = true;
+
+    setTimeout(function() {
+      window.location.href = "bmi-calculator.html"
+    }, 100);
+  });
 // BMI FORMULA - BMI CALCULATING
 
 const bmiCalculation = () => {
@@ -34,18 +61,34 @@ const bmiCalculation = () => {
   finalBmi = weightInput.value / bmiFirstPart * 100;
   finalBmi = Math.floor(finalBmi);
   console.log(finalBmi)
+
+  if(finalBmi >= 25 && finalBmi <= 30) {
+    calculatorDiv.innerHTML = `You might want to lose a few kgs! Your BMI is: ${finalBmi}`
+    calculatorDiv.appendChild(calculateAgainBtn);
+  }
+
+  if(finalBmi >= 30) {
+    calculatorDiv.innerHTML = `You are obese, you need to lose your weight. Your BMI is: ${finalBmi}`
+    calculatorDiv.appendChild(calculateAgainBtn);
+  }
+
+  if(finalBmi <= 25 && finalBmi >= 19) {
+    calculatorDiv.innerHTML = `You have a healthy bmi, congratulations! Your BMI is: ${finalBmi}`
+    calculatorDiv.appendChild(calculateAgainBtn);
+  }
+
+  if(finalBmi < 19 && finalBmi >= 15) {
+    calculatorDiv.innerHTML = `You are underweight, gain some weight. Your BMI is: ${finalBmi}`
+    calculatorDiv.appendChild(calculateAgainBtn);
+  }
+
+  if(finalBmi < 15) {
+    calculatorDiv.innerHTML = `You have to start eating. It's dangerous for your health! Your BMI is: ${finalBmi}`
+    calculatorDiv.appendChild(calculateAgainBtn);
+  }
 }
 
-
-//
 calculateBtn.addEventListener("click", bmiCalculation)
-
-
-
-
-
-
-
 
 
 
